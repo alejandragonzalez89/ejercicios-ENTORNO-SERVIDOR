ae3c24985ac358270b096bbedb9cacfa247155ea
Alejandra <``ale.89gm@gmail.com>
2021-11-17 12:54:52 +0100

    ``pruebas´´

---
+++ blog.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="estiloprueba.css">
+    <title>caso 1 BLOG probando servidor</title>
+  </head>
+  <body>
+    <div id=container>
+      <header>
+        <?php
+        echo "pag BLOG";
+        ?>
+
+      </header>
+
+      <aside>
+        MENU
+        <ul>
+          <li> <a href="intranet.php"> PAG INTRANET </a> </li>
+          <li> <a href="blog.php"> PAG BLOG </a></li>
+        </ul>
+      </aside>
+
+      <main>
+        <p>esto es un ejemplo de funcionamiento de una pagina probando el servidor
+          Para la realización de esta tarea seguiremos los siguientes pasos:
+          1.	Usa la documentación de la unidad 2.
+          2.	Crearemos una primera página que se denomine intranet.php
+          3.	Crearemos una segunda página que se denomine blog.php
+          4.	En ambas páginas crearemos los siguientes apartados:
+          i.	Menú con enlaces a las dos páginas
+          ii.	Título de la página (Escrito con PHP)
+          iii.	Un pequeño párrafo descriptivo
+        </p>
+      </main>
+
+    </div>
+
+
+  </body>
+</html>
